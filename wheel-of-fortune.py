e9ab2c8b81ba3e7d9fd0ee7acf395248a4d07dee
stuckerdev10 <96314244+stuckerdev10@users.noreply.github.com>
2021-12-28 15:11:15 -0600

    Add files via upload
    Rearranged functions to let script actually work

--- wheel-of-fortune.py
+++ wheel-of-fortune.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[286]:
+# In[305]:
 
 
 import random #importing packages and initializing values for the game
@@ -12,14 +12,14 @@ finalscoreList = [0, 0, 0]
 vowels = {'a', 'e', 'i', 'o', 'u'}
 
 
-# In[287]:
+# In[306]:
 
 
 def has_numbers(string): #this function helps get rid of words with numbers in them
     return any(char.isdigit() for char in string)
 
 
-# In[288]:
+# In[307]:
 
 
 answerFile = open("words-list.txt", 'r')
@@ -39,6 +39,185 @@ for i in range(0, len(answerList) - 1):#converts everything to lowercase
 
 
 # In[ ]:
+
+
+def playersTurn(player):#core gameplay of the first to rounds, takes int of player number to start the round
+    global playerScoreDict#bring in score an spin values
+    global spinValues
+    selectedLetters = []#keeps track of letters already selected
+    playing = True 
+    while playing == True:#loop to keep the round perpetually going
+        print(f"Player {player}'s turn: ")
+        print(playingField)
+        letterCounter = 0#initializes scoring/answer variables before round
+        spinScore = random.choice(spinValues)
+        totalAttemptScore = 0
+        print(f"Your Rolled: {spinScore}")
+        if(spinScore) == 0:#if bankrupt
+            print("You went Bankrupt! You lost all your money")
+            playerScoreDict[player] = 0
+            if player + 1 > 3:#moves to next player, resets to player one if already at 3
+                player = 1
+            else:
+                player = player + 1
+        elif spinScore == 'Lose a Turn':#if round lost, moves to next player without losing money
+            print("You lost this turn!")
+            if player + 1 > 3:
+                player = 1
+            else:
+                player = player + 1
+        else:
+            print(f"Letters already chosen: {selectedLetters}")
+            print('Select a letter or answer the puzzle:')
+            attempt = input()#player inputs guess or answers puzzle
+            if len(attempt) > 1:#if the player chooses to answer
+                answerCounter = 0
+                attemptWordList = list(attempt)#breaks attempt into list for iteration
+                for count, letter in enumerate(attemptWordList):#count if each letter in the attempt matches the answers letter
+                    if attemptWordList[count] == chosenAnswerList[count]:
+                        answerCounter += 1
+                    else:#if there is a mismatch, move to next player
+                        print("Incorrect word")
+                        if player + 1 > 3:
+                            player = 1
+                        else:
+                            player = player + 1
+                if answerCounter == len(chosenAnswerList):#if each letter matches the answer
+                    print("Congratulations! You chose the correct word")
+                    finalscoreList[player - 1] = playerScoreDict[player]#players score is saved to the final score
+                    break
+            else:#if they are guessing just a letter
+                letterCounter = 0#tracks how many times a letter appears
+    
+                selecting = True
+
+                while selecting == True:#while loop to let player continue to guess in case of error
+
+                    if attempt in selectedLetters:#if the letter was already chosen, continue to next player
+                        print("Letter already chosen")
+                        break
+                    else:
+                        if attempt in vowels:#if letter is in vowels set
+                            print('You bought a vowel for $250')
+                            if playerScoreDict[player] < 250:#if player can't afford a vowel, cancel and move to next player
+                                print("Sorry, you don't have enough money to buy a vowel")
+                                if player + 1 > 3:
+                                    player = 1
+                                else:
+                                    player = player + 1
+                                break
+                            else:#if they can afford, add to selectedLetters and subtrack 250 from their score
+                                selectedLetters.append(attempt)
+                                playerScoreDict[player] = playerScoreDict[player] - 250
+                            if attempt in chosenAnswerList:#if letter exists in the answer
+                                print('Correct Choice!')
+                            else:
+                                print("Incorrect choice")#if its the wrong letter, move on to next player
+                                if player + 1 > 3:
+                                    player = 1
+                                else:
+                                    player = player + 1
+                                break
+                            for count, letter in enumerate(chosenAnswerList):#keeps track of how many times letter appears,
+                                if chosenAnswerList[count] == attempt:
+                                    letterCounter += 1
+                                    playingField[count] = attempt#replaces underscores on playing field with the letter
+                            totalAttemptScore = letterCounter * spinScore#calculates total score of attempt
+                            playerScoreDict[player] = playerScoreDict[player] + totalAttemptScore
+                            print(f"This letter appeared {letterCounter} times for a score of {totalAttemptScore}")
+                            print(f"Your new score is {playerScoreDict[player]}")
+                            print(playingField)
+                        else:#if the guess is a constonant
+                            print('this is a constonant')
+                            if attempt in chosenAnswerList:#if correct move on
+                                selectedLetters.append(attempt)
+                                print('Correct Choice!')
+                            else:
+                                selectedLetters.append(attempt)#if incorrect move to next player
+                                print('Incorrect choice')
+                                if player + 1 > 3:
+                                    player = 1
+                                else:
+                                    player = player + 1
+                                break
+                            for count, letter in enumerate(chosenAnswerList):#same scoring scheme as before
+                                if chosenAnswerList[count] == attempt:
+                                    letterCounter += 1
+                                    playingField[count] = attempt
+                            totalAttemptScore = letterCounter * spinScore
+                            playerScoreDict[player] = playerScoreDict[player] + totalAttemptScore
+                            print(f"This letter appeared {letterCounter} times for a score of {totalAttemptScore}")
+                            print(playingField)
+                    print(f"Scores: {playerScoreDict}")
+
+
+# In[ ]:
+
+
+def finalRound(player):#final round of the game, player with max score plays
+    global vowels
+    print(f"Congratulations player {player}! You have made it to the final round")
+    print("This next round is worth $1,000,000.")
+    print("Here is the word: ")
+    chosenAnswer = random.choice(answerList)#choose new word
+    chosenAnswerList = list(chosenAnswer)
+    playingField = []
+    for letter in chosenAnswerList:
+        playingField.append('_')
+    print(playingField)#display word to player
+    print("Here is the word after the default clues (r s t l n e)")
+    defaultClues = ['r', 's', 't', 'l', 'n' 'e']
+    for count, letter in enumerate(chosenAnswerList):#this loop checks each letter in chosenAnswerList against the default clues
+        for letter in defaultClues:
+            if letter == chosenAnswerList[count]:
+                playingField[count] = letter#replaces the underscore with the letter if they match
+                break
+    print(playingField)
+    guessSelections = []
+    for i in range(1, 4):#player chooses 3 constonants, which gets put in guessSelections List
+        print(f"Select constonant {i}")
+        attempt = input()
+        if len(attempt) > 1 or attempt in vowels:#checks to see if constonant in proper format
+            while len(attempt) > 1 or attempt in vowels:
+                print("invalid format, please enter again")
+                attempt = input()
+        guessSelections.append(attempt)
+    print("Now choose a vowel: ")
+    attempt = input()#user must choose a vowel
+    if attempt not in vowels:#checks to see if letter is actually a vowel
+        while attempt not in vowels:
+            print("Not a vowel, please try again: ")
+            attempt = input()
+    guessSelections.append(attempt)
+    print(f"Here are your selections: {guessSelections}")
+    print("Here is the new playing field: ")
+    for count, letter in enumerate(chosenAnswerList):#runs through letters against chosenAnswerList
+        for letter in guessSelections:
+            if letter == chosenAnswerList[count]:
+                playingField[count] = letter
+                break
+    print(playingField)
+    print("For $1,000,000 dollars, what is the word?")
+    attempt = input()#player inputs guess
+    answerCounter = 0
+    attemptWordList = list(attempt)
+    for count, letter in enumerate(attemptWordList):#runs through same word guessing progress as previous rounds
+        if attemptWordList[count] == chosenAnswerList[count]:
+            answerCounter += 1
+        else:
+            print("Incorrect word")
+            break
+    if answerCounter == len(chosenAnswerList):
+        print("Congratulations! You chose the correct word")
+
+
+# In[ ]:
+
+
+
+
+
+# In[308]:
 
 
 print("-------------------------------------")#begin first round
